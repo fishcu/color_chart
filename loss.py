fbd8cc16f385587bf2340b84caa3f43957a1e6cb
fishku <isaacbd@gmail.com>
2024-01-23 21:29:05 +0100

    Normalize loss

--- loss.py
+++ loss.py
@@ -5,7 +5,7 @@ def loss(X, Y, D, w):
     dw = np.zeros((Y.shape[0], 3))
     for d in range(3):
         dw[:, d] = np.matmul(D[:, :, d], w)
-    return np.sum((Y - X - dw)**2)  # + lambda_s * np.sum(w**2)
+    return np.sum((Y - X - dw)**2) / Y.shape[0]  # + lambda_s * np.sum(w**2)
 
 
 def loss_no_correction(X, Y):
